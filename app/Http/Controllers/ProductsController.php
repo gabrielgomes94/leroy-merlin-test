cb1fe30f5bf53172b9b1bd57ffc182e8c8bcab4c
Gabriel Gomes <gabrielgomesch@gmail.com>
2019-05-02 19:42:55 -0300

    feature(product): action to store products
    - gets the file url, download it to the filesystem, them call the Excel Importer class

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use App\Imports\ProductsImport;
+use App\Jobs\ImportSpreadsheet;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ProductsController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        dispatch(new ImportSpreadsheet($request->input('file_url')));
+
+        return response()->json('A planilha está sendo importada.', 201);
+    }
 }
