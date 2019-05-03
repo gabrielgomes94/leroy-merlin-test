2bb0c81a9165a60439bc1518f06984f3946a0d9d
Gabriel Gomes <gabrielgomesch@gmail.com>
2019-05-03 15:59:40 -0300

    feature(products): index endpoint for products

--- tests/Feature/ProductsControllerTest.php
+++ tests/Feature/ProductsControllerTest.php
@@ -66,4 +66,35 @@ class ProductsControllerTest extends TestCase
         $response = $this->json('GET', '/api/products/import_status/'.$job_status_id, [], $headers)
             ->assertStatus(200);
     }
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testAllProducts()
+    {
+        factory(Product::class)->create([
+            'lm' => '123456',
+            'name' => 'First Product'
+        ]);
+
+        factory(Product::class)->create([
+            'lm' => '234567',
+            'name' => 'Second Product'
+        ]);
+
+        $auth_token = "p2lbgWkFrykA4QyUmpHihzmc5BNzIABq";
+        $headers = ['Authorization' => "$auth_token"];
+
+        $response = $this->json('GET', '/api/products', [], $headers)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                '*' => ['id', 'lm', 'name', 'free_shipping', 'description', 'price', 'category', 'created_at', 'updated_at'],
+            ]);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
 }
