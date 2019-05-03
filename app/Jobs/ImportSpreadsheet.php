fd495d1c63abb2915e7a1186b465e29670af2543
Gabriel Gomes <gabrielgomesch@gmail.com>
2019-05-02 21:52:10 -0300

    feature(import): job to import spreadsheet

---
+++ app/Jobs/ImportSpreadsheet.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Imports\ProductsImport;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Storage;
+
+class ImportSpreadsheet implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $file_url;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($file_url)
+    {
+        $this->file_url = $file_url;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $filename = $this->saveFile($this->file_url);
+        $import = new ProductsImport();
+        $import->onlySheets('Plan1');
+        Excel::import($import, $filename);
+    }
+
+    private function saveFile($file_url)
+    {
+        $contents = file_get_contents($file_url);
+        $name = substr($file_url, strrpos($file_url, '/') + 1);
+        Storage::put($name, $contents);
+
+        return $name;
+    }
+}
