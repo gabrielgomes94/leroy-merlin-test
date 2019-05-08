6ae9670048f4298df8383eb49a3c4a6bbccdcf7a
Gabriel Gomes <gabrielgomesch@gmail.com>
2019-05-08 10:20:33 -0300

    test(product): endpoint to check import status

--- tests/Feature/ProductsControllerTest.php
+++ tests/Feature/ProductsControllerTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Queue;
+use Imtigger\LaravelJobStatus\JobStatus;
 
 class ProductsControllerTest extends TestCase
 {
@@ -27,4 +28,42 @@ class ProductsControllerTest extends TestCase
         Queue::assertPushed(ImportSpreadsheet::class, 1);
     }
 
+    public function testsProductsCheckImportStatusWhenJobFinished()
+    {
+        $auth_token = "p2lbgWkFrykA4QyUmpHihzmc5BNzIABq";
+        $headers = ['Authorization' => "$auth_token"];
+
+        $status = 'finished';
+        $job_status_id = JobStatus::create(['type' => 'App\Jobs\ImportSpreadsheet', 'status'=>$status])->id;
+        $response = $this->json('GET', '/api/products/import_status/'.$job_status_id, [], $headers)
+            ->assertStatus(201);
+    }
+
+    public function testsProductsCheckImportStatusWhenJobFailed()
+    {
+        $auth_token = "p2lbgWkFrykA4QyUmpHihzmc5BNzIABq";
+        $headers = ['Authorization' => "$auth_token"];
+
+        $status = 'failed';
+
+        $job_status_id = JobStatus::create(['type' => 'App\Jobs\ImportSpreadsheet', 'status'=>$status])->id;
+        $response = $this->json('GET', '/api/products/import_status/'.$job_status_id, [], $headers)
+            ->assertStatus(500);
+    }
+
+    public function testsProductsCheckImportStatusWhenJobQueuedOrExecuting()
+    {
+        $auth_token = "p2lbgWkFrykA4QyUmpHihzmc5BNzIABq";
+        $headers = ['Authorization' => "$auth_token"];
+
+        $status = 'queued';
+        $job_status_id = JobStatus::create(['type' => 'App\Jobs\ImportSpreadsheet', 'status'=>$status])->id;
+        $response = $this->json('GET', '/api/products/import_status/'.$job_status_id, [], $headers)
+            ->assertStatus(200);
+
+        $status = 'executing';
+        $job_status_id = JobStatus::create(['type' => 'App\Jobs\ImportSpreadsheet', 'status'=>$status])->id;
+        $response = $this->json('GET', '/api/products/import_status/'.$job_status_id, [], $headers)
+            ->assertStatus(200);
+    }
 }
